5dbf676c28faaf9e60c0e5010b9daacad45d9dec
Eisson Alipio <eisson.alipio@gmail.com>
2018-11-24 23:36:05 -0500

    feat: show message from api in mobile page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,24 @@
-import { Component } from "@angular/core";
-import { ApiService } from "./api.service";
-
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+interface IResponse {
+    message: string
+}
 @Component({
-    selector: "ns-app",
-    moduleId: module.id,
-    templateUrl: "./app.component.html",
+  selector: 'ns-app',
+  moduleId: module.id,
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-    data: any;
-    constructor(private api: ApiService){}
-    ngOnInit() {
-         this.getData();
-         this.data.subscribe(data => {
-            console.log('---------------------------------------------', data.message);
-         });
-    }
-    getData() : void {
-        this.data = this.api.getData();
-    }
 
- }
+export class AppComponent {
+  data: any;
+  constructor(private api: ApiService) {}
+  ngOnInit() {
+    this.getData();
+  }
+  getData(): void {
+    this.api.getData().subscribe((response: IResponse) => {
+      this.data = response;
+      console.log(response.message);
+    });
+  }
+}
